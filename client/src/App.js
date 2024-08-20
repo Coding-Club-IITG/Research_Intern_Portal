a2638437679c922d5209435c2294554b90a066ee
RajNandani31 <rajnandani185@gmail.com>
2024-08-20 22:59:30 +0530

    created upload

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,9 @@
-import { DatePicker } from 'antd';
+import Cv from "./pages/profile/Cv";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-6xl">App</h1>
-      <DatePicker />
+      <Cv />
     </div>
   );
 }
