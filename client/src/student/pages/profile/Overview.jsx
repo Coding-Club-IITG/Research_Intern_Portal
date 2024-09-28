209a2c3a336eee06d9bed0d7099385923e7442ad
WebDevPranjal <vpranjal291003@gmail.com>
2024-09-28 23:07:39 +0530

    removed uneccesary css

--- client/src/student/pages/profile/Overview.jsx
+++ client/src/student/pages/profile/Overview.jsx
@@ -45,12 +45,12 @@ const Overview = () => {
 
   return (
     <div>
-      <div className="bg-white p-4 rounded-lg mb-4">
+      <div className="py-4">
         <div className="font-semibold">What recruiters will see</div>
       </div>
-      <div className="max-w-4xl mx-auto p-6 bg-white rounded-lg mt-6 border border-gray-300">
+      <div className="max-w-4xl mx-auto p-6 rounded-lg mt-6 border border-gray-300">
         <div className="flex items-center mb-4">
-          <div className="w-32 h-32 rounded-full overflow-hidden bg-gray-200 flex items-center justify-center mr-6">
+          <div className="w-32 h-32 rounded-full overflow-hiddenflex items-center justify-center mr-6">
             <img
               src={profileData.profilePicUrl}
               alt="Profile Pic"
@@ -61,7 +61,7 @@ const Overview = () => {
             <h1 className="text-3xl font-semibold text-gray-800">
               {profileData.name}
             </h1>
-            <p className="text-gray-600 mt-1 bg-gray-100 inline-block p-1 rounded-md">
+            <p className="text-gray-600 mt-1 inline-block p-1 rounded-md">
               {profileData.activityStatus}
             </p>{" "}
             {/* Active Today */}
@@ -98,7 +98,7 @@ const Overview = () => {
           </div>
         </div>
 
-        <div className="bg-white p-2 rounded-lg mb-2">
+        <div className="p-2 rounded-lg mb-2">
           <h2 className="text-gray-500 mb-2">Looking for</h2>
           <p className="black">{profileData.lookingFor}</p>
         </div>
@@ -112,40 +112,40 @@ const Overview = () => {
           </p>
         </div>
 
-        <div className="bg-white p-2 rounded-lg mb-2">
+        <div className="p-2 rounded-lg mb-2">
           <h3 className="text-lg font-semibold text-gray-800 mb-2">
             Ideal Next Opportunity
           </h3>
           <h2 className="text-gray-500 mb-2">Desired Salary</h2>
-          <p className="text-gray-600 bg-gray-100 inline-block p-1 mb-2 rounded-md">
+          <p className="text-gray-600 inline-block p-1 mb-2 rounded-md">
             {" "}
             {profileData.idealNextOpportunity.desiredSalary}
           </p>
           <h2 className="text-gray-500 mb-2">Desired role</h2>
-          <p className="text-gray-600 bg-gray-100 inline-block p-1 mb-2 rounded-md">
+          <p className="text-gray-600 inline-block p-1 mb-2 rounded-md">
             {" "}
             {profileData.idealNextOpportunity.desiredRole}
           </p>
           <h2 className="text-gray-500 mb-2">Remote Work</h2>
-          <p className="text-gray-600 bg-gray-100 inline-block p-1 mb-2 rounded-md">
+          <p className="text-gray-600 inline-block p-1 mb-2 rounded-md">
             {" "}
             {profileData.idealNextOpportunity.remoteWork}
           </p>
           <h2 className="text-gray-500 mb-2">Desired Location</h2>
-          <p className="text-gray-600 bg-gray-100 inline-block p-1 rounded-md">
+          <p className="text-gray-600 inline-block p-1 rounded-md">
             {" "}
             {profileData.idealNextOpportunity.desiredLocation}
           </p>
         </div>
 
-        <div className="bg-white p-2 rounded-lg mb-2">
+        <div className="p-2 rounded-lg mb-2">
           <h4 className="text-gray-500 mb-2">Desired Tech Stack</h4>
           <div className="flex flex-wrap gap-4">
             {profileData.idealNextOpportunity.desiredTechStack.map(
               (tech, index) => (
                 <div
                   key={index}
-                  className="bg-gray-100 p-1 rounded-md shadow-sm"
+                  className="p-1 rounded-md shadow-sm"
                 >
                   <p className="text-gray-600">{tech}</p>
                 </div>
@@ -154,7 +154,7 @@ const Overview = () => {
           </div>
         </div>
 
-        <div className="bg-white p-2 rounded-lg mb-2">
+        <div className="p-2 rounded-lg mb-2">
           <h4 className="text-gray-500 mb-2">Wants</h4>
           <ul className="list-disc list-inside">
             {profileData.idealNextOpportunity.wants.map((want, index) => (
