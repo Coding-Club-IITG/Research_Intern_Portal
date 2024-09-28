147b95e7edaae649845237054d6d68648d7aade7
Pavan Lokesh <pavanlokesh.developer@gmail.com>
2024-09-28 22:43:23 +0530

    Admin page merged with master

--- client/src/admin/pages/home/jobs.jsx
+++ client/src/admin/pages/home/jobs.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Flex, Tabs } from 'antd';
 import {Link, useParams,Outlet} from 'react-router-dom';
+
 import Dashboard from './components/Dashboard.jsx'
 import Jobscontent from './components/Jobscontent.jsx';
 import Professorcontent from './components/Professorcontent.jsx';
@@ -10,15 +11,15 @@ const onChange = (key) => {
   console.log(key);
 };
 const items = [
-  {
-    key: '0',
-    children:<Dashboard/>,
-    label: <Link to='/admin'>Dash Board</Link>  
-
-  },
+    {
+        key: '0',
+        children: <Dashboard/>,
+        label: <Link to='/admin'>Dash Board</Link>
+    
+      },
   {
     key: '1',
-    children:<Professorcontent/>,
+    children: <Professorcontent/>,
     label: <Link to='/admin/professor'>Professor Details</Link>
 
   },
@@ -29,12 +30,11 @@ const items = [
   },
   {
     key: '3',
-    label: <Link to='/student/jobs'>Job Details</Link>,
+    label: <Link to='/admin/jobs'>Job Details</Link>,
     children: <Jobscontent/>,
   },
 
 ];
 
-const home = () => <Tabs style = {{marginLeft:'30px'  }}defaultActiveKey="0" items={items} onChange={onChange} />;
-export default home;
-
+const Job = () => <Tabs style = {{marginLeft:'30px'  }}defaultActiveKey="3" items={items} onChange={onChange} />;
+export default Job;
