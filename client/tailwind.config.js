897f9fd04515dd8931f520fadb47c95a262b95ca
WebDevPranjal <vpranjal291003@gmail.com>
2024-09-28 22:38:52 +0530

    Add theme colors

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,7 +3,17 @@ module.exports = {
   darkMode: "class",
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
-    extend: {},
+    extend: {
+      theme: {
+        colors: {
+          darkBorder: '#52526B',
+          darkBgPrimary: '#16162A',
+          darkBgSecondary: '#292946',
+          darkTextPrimary: '#4277FF',
+          lightTextPrimary: '#4277FF',
+        }
+      }
+    },
   },
   plugins: [],
 };
