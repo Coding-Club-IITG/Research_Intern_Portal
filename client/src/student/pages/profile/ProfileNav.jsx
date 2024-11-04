294668a10396288d87552f886b8d6443327093c5
RajNandani31 <rajnandani185@gmail.com>
2024-11-04 10:14:40 +0530

    overview dark  mode created

--- client/src/student/pages/profile/ProfileNav.jsx
+++ client/src/student/pages/profile/ProfileNav.jsx
@@ -2,18 +2,19 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 const App = () => {
   return (
-    <div className="bg-white">
-      <h1 className="text-3xl font-bold mb-4">Edit your Profile</h1>
-      <div className="border-b border-gray-300 mb-6">
+    <div className="bg-white dark: bg-slate-700">
+      <h1 className="text-3xl font-bold mb-4 dark:text-white">Edit your Profile</h1>
+      <div className="border-b border-gray-300 mb-6 dark:border-yellow-400">
         <ul className="flex gap-8 font-xl mt-6 mb-2">
           <li>
             <NavLink
               to="overview"
               className={({ isActive }) =>
-                isActive ? "text-blue-700 border-b-2 pb-2 border-blue-700" : "text-gray-600"
+                isActive
+                  ? "text-blue-700 border-b-2 pb-2 border-blue-700 dark: text-yellow-400 dark:border-yellow-400"
+                  : "text-gray-600 dark:text-white"
               }
-              exact
-            >
+              exact>
               Overview
             </NavLink>
           </li>
@@ -21,9 +22,10 @@ const App = () => {
             <NavLink
               to={`edit`}
               className={({ isActive }) =>
-                isActive ? "text-blue-700 border-b-2 pb-2 border-blue-700" : "text-gray-600"
-              }
-            >
+                isActive
+                  ? "text-blue-700 border-b-2 pb-2 border-blue-700 dark: text-yellow-400 dark:border-yellow-400"
+                  : "text-gray-600 dark:text-white"
+              }>
               Edit Profile
             </NavLink>
           </li>
@@ -31,9 +33,10 @@ const App = () => {
             <NavLink
               to={`resume`}
               className={({ isActive }) =>
-                isActive ? "text-blue-700 border-b-2 pb-2 border-blue-700" : "text-gray-600"
-              }
-            >
+                isActive
+                  ? "text-blue-700 border-b-2 pb-2 border-blue-700 dark: text-yellow-400 dark:border-yellow-400"
+                  : "text-gray-600 dark:text-white"
+              }>
               Resume/CV
             </NavLink>
           </li>
--- client/src/student/pages/profile/StudentCv.jsx
+++ client/src/student/pages/profile/StudentCv.jsx
@@ -2,13 +2,13 @@ import Upload from "./Upload";
 
 function Cv() {
   return (
-    <div className="flex flex-row border border-gray-300 m-4 flex-wrap">
+    <div className="flex flex-row border border-gray-300 dark: border-yellow-400 m-4 flex-wrap">
       <div className="basis-1/4 grow m-6 shrink">
-        <p className="font-semibold text-lg ">Upload your recent resume or CV</p>
+        <p className="font-semibold text-lg dark: text-white">Upload your recent resume or CV</p>
 
-        <p className=" opacity-80">Upload your most up-to-date resume</p>
+        <p className=" opacity-80 dark: text-gray-300">Upload your most up-to-date resume</p>
 
-        <p className="opacity-80">File types: DOC, DOCX, PDF, TXT</p>
+        <p className="opacity-80 dark: text-gray-300">File types: DOC, DOCX, PDF, TXT</p>
       </div>
       <div className="basis-1/2 grow shrink m-8">
         <Upload />
