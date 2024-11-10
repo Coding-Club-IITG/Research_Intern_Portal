aa7ddb864cd7f0408137146cfc65b32cdbf65b2d
aditya-samal <samaladitya2004@gmail.com>
2024-11-10 14:46:41 +0530

    Deleted Node modules

--- client/src/student/pages/internships/Filter.jsx
+++ client/src/student/pages/internships/Filter.jsx
@@ -2,61 +2,61 @@ import React, { useEffect } from "react";
 import useLocalStorage from "../../../hooks/useLocalStorage";
 
 function Filter({ onSearch }) {
-  const [searchTerm, setSearchTerm] = useLocalStorage("searchTerm", "");
-  const [department, setDepartment] = useLocalStorage("department", "");
+  const [prof, setProf] = useLocalStorage("prof", "");
+  // const [department, setDepartment] = useLocalStorage("department", "");
   const [role, setRole] = useLocalStorage("role", "");
 
-  const handleSearch = (updatedSearchTerm, updatedDepartment, updatedRole) => {
+  const handleSearch = (updatedProf, updatedRole) => {
     if (onSearch) {
       onSearch({
-        searchTerm: updatedSearchTerm ?? searchTerm,
-        department: updatedDepartment ?? department,
+        prof: updatedProf ?? prof,
+        // department: updatedDepartment ?? department,
         role: updatedRole ?? role
       });
     }
   };
 
   useEffect(() => {
-    handleSearch(searchTerm, department, role);
-  }, [searchTerm, department, role]);
+    handleSearch(prof, role);
+  }, [prof, role]);
 
-  const handleSearchTermChange = (e) => {
+  const handleProfChange = (e) => {
     const value = e.target.value;
-    setSearchTerm(value);
-    handleSearch(value, department, role);
+    setProf(value);
+    handleSearch(value, role);
   };
 
-  const handleDepartmentChange = (e) => {
-    const value = e.target.value;
-    setDepartment(value);
-    handleSearch(searchTerm, value, role);
-  };
+  // const handleDepartmentChange = (e) => {
+  //   const value = e.target.value;
+  //   setDepartment(value);
+  //   handleSearch(prof, value, role);
+  // };
 
   const handleRoleChange = (e) => {
     const value = e.target.value;
     setRole(value);
-    handleSearch(searchTerm, department, value);
+    handleSearch(prof, value);
   };
 
   return (
     <div className="bg-gray-50 dark:bg-slate-700 p-4 rounded-md mb-6 flex items-center gap-4 max-sm:flex-col">
       <input
         type="text"
-        placeholder="Search for a job title..."
+        placeholder="Professor"
         className="border border-gray-300 dark:border-yellow-300 rounded-md px-4 py-2 w-full text-gray-800 dark:text-white bg-white dark:bg-slate-700 focus:outline-none focus:ring dark:focus:ring-yellow-400"
-        value={searchTerm}
-        onChange={handleSearchTermChange}
+        value={prof}
+        onChange={handleProfChange}
       />
-      <input
+      {/* <input
         type="text"
         placeholder="Department"
         className="border border-gray-300 dark:border-yellow-300 rounded-md px-4 py-2 w-full text-gray-800 dark:text-white bg-white dark:bg-slate-700 focus:outline-none focus:ring dark:focus:ring-yellow-400"
         value={department}
         onChange={handleDepartmentChange}
-      />
+      /> */}
       <input
         type="text"
-        placeholder="Role"
+        placeholder="Job Title"
         className="border border-gray-300 dark:border-yellow-300 rounded-md px-4 py-2 w-full text-gray-800 dark:text-white bg-white dark:bg-slate-700 focus:outline-none focus:ring dark:focus:ring-yellow-400"
         value={role}
         onChange={handleRoleChange}
