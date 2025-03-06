6957096dcda752b2eec0dc2cf2efafe3d825ca0e
WebDevPranjal <vpranjal291003@gmail.com>
2025-03-06 18:06:05 +0530

    change mogodb_url for testing

--- server/src/config/db_config.js
+++ server/src/config/db_config.js
@@ -3,14 +3,9 @@ import data from "./server_config.js";
 import logger from "../utils/logger.js";
 async function connectToDb() {
   try {
-    if (process.env.NODE_ENV == "development") {
-      await mongoose.connect(data.DB_URL);
-      console.log("Connection established!");
-      logger.info("connection established!");
-    } else {
-      logger.info("We are not ready with the other url");
-      console.log("we are not ready with the other url");
-    }
+    await mongoose.connect(data.DB_URL);
+    console.log("Connection established!");
+    logger.info("connection established!");
   } catch (error) {
     logger.error(`unable to connect to db , ${error}`);
     console.log("unable to connect to db", error);
