7deb7747a12ee9b254063c5e8be5514be9fd7c6a
WebDevPranjal <vpranjal291003@gmail.com>
2025-03-06 20:16:39 +0530

    add frontend url in cors

--- server/src/index.js
+++ server/src/index.js
@@ -26,7 +26,7 @@ setupSwagger(app);
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["http://localhost:3000", "https://rip.codingclubiitg.in"],
     credentials: true,
   })
 );
