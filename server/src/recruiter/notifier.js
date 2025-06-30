2fa487ae87d71e8c6153af0b9f3c5e4b37f847c9
maydayv7 <maydayv7@gmail.com>
2025-06-30 21:43:16 +0530

    Fix jobExpiryNotifier

--- server/src/recruiter/notifier.js
+++ server/src/recruiter/notifier.js
@@ -2,21 +2,23 @@ import cron from "node-cron";
 import axios from "axios";
 import Jobs from "./models/jobs.js";
 import recruiter from "./models/recruiter.js";
+import logger from "../utils/logger.js";
 
 export default function jobExpiryNotifier() {
+  logger.info("jobExpiryNotifier started");
+
   // Runs every 24 hours
   cron.schedule("0 0 * * *", async () => {
     logger.info("Checking for jobs expiring within 24 hours...");
 
     const now = new Date();
-    const after23hrs = new Date(now.getTime() + 23 * 60 * 60 * 1000);
     const after24hrs = new Date(now.getTime() + 24 * 60 * 60 * 1000);
 
     try {
       const expiringJobs = await Jobs.find({
         accepting: true,
         last_date: {
-          $gte: after23hrs,
+          $gte: now,
           $lte: after24hrs,
         },
       });
@@ -26,7 +28,7 @@ export default function jobExpiryNotifier() {
         const notificationResponse = await axios.post(
           `${process.env.NOTIFICATION_URL}/create-students`,
           {
-            title: `"${job.title}" is closing soon!`,
+            title: `Internship is closing soon!`,
             message: `Applications for "${job.title}" by ${recruiter_data.name} will close within 24 hours.\nClick on "View More" to know more about the internship.`,
             link: `/student/internships/internship/${job._id}`,
           },
